70c6ce93623f65b0e17a2901797996547fa236f7
birme <jonas.birme@eyevinn.se>
2021-10-15 09:24:14 +0200

    ci: check that it works as a promise

--- index.test.ts
+++ index.test.ts
@@ -4,3 +4,11 @@ test("Should fetch HLS and calculate correct duration", async () => {
   const duration = await hlsduration(new URL("https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8"));
   expect(duration).toEqual(105.72000000000001);
 });
+
+test("Should work as a promise", (done) => {
+  hlsduration(new URL("https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8"))
+  .then(duration => {
+    expect(duration).toEqual(105.72000000000001);    
+    done();
+  });
+});
